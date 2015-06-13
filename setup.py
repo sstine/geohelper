71bf82394b2797f0172669bbfca3f2211c0459c1
Samuel Stine <sstine93@gmail.com>
2015-06-13 18:33:12 -0400

    Updated version information to comply with pep

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 5 - Initial version',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
